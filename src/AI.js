05477954da63faae741fe606754b1b41dd753928
Yates <kevinmyates@gmail.com>
2020-12-25 20:09:44 -0800

    Added componentDidUpdate and win conditions

---
+++ src/AI.js
@@ -0,0 +1,93 @@
+
+// AIGetRandomEmptyCellIndex
+export function Easy(board) {
+    let openCells = [];
+
+    for (let index = 0; index < (board).length; index++) {
+        if (board[index] === '') {
+            openCells.push(index);
+        }
+    }
+
+    const randomIndex = Math.floor(Math.random() * openCells.length);
+
+    return openCells[randomIndex];
+}
+
+export function Medium(board) {
+    // take middle cell
+    if (board[4] === "") {
+        return 4;
+    }
+
+    let boardCellValues = [0, 0, 0, 
+        0, 0, 0, 
+        0, 0, 0];
+
+    // check diaginal for matchs
+    // upper left to lower right diagonal check 
+    if (board[0] === board[4] || board[0] === board[8] || board[4] === board[8]) {
+        if (board[0] === '') {
+            boardCellValues[0] += 1;
+        }
+        if (board[4] === '') {
+            boardCellValues[4] += 1;
+        }
+        if (board[8] === '') {
+            boardCellValues[8] += 1;
+        }
+    }
+    
+    // upper right to lower left diagonal check
+    if (board[2] === board[4] || board[2] === board[6] || board[4] === board[6]) {
+        if (board[2] === '') {
+            boardCellValues[2] += 1;
+        }
+        if (board[4] === '') {
+            boardCellValues[4] += 1;
+        }
+        if (board[6] === '') {
+            boardCellValues[6] += 1;
+        }
+    }
+
+    // check horizontal for matches
+    for (let index = 0; index < 3; index++) {
+        let a = board[0 + (index * 3)];
+        let b = board[1 + (index * 3)];
+        let c = board[2 + (index * 3)];
+
+        if (a === b || a === c || b === c) {
+            if (board[0 + (index * 3)] === '') {
+                boardCellValues[0 + (index * 3)] += 1;
+            }
+            if (board[1 + (index * 3)] === '') {
+                boardCellValues[1 + (index * 3)] += 1;
+            }
+            if (board[2 + (index * 3)] === '') {
+                boardCellValues[2 + (index * 3)] += 1;
+            }
+        }
+    }
+
+    // check vertical for matches
+    for (let index = 0; index < 3; index++) {
+        let a = board[0 + index];
+        let b = board[3 + index];
+        let c = board[6 + index];
+
+        if (a === b || a === c || b === c) {
+            if (board[0 + index] === '') {
+                boardCellValues[0 + index] += 1;
+            }
+            if (board[3 + index] === '') {
+                boardCellValues[3 + index] += 1;
+            }
+            if (board[6 + index] === '') {
+                boardCellValues[6 + index] += 1;
+            }
+        }
+    }
+
+    return boardCellValues.indexOf(Math.max(...boardCellValues));
+}
--- src/components/TicTacToe.js
+++ src/components/TicTacToe.js
@@ -4,35 +4,40 @@ import GameInfo from "./GameInfo"
 import Player from "./Player"
 import GameBoard from "./GameBoard"
 import "../index.css";
+import * as AI from "../AI.js"
 
 const buildInitialState = () => ({
     Players: [
-        {id: 0,
-        name: "Player 1",
-        marker: "X",
-        humanOrAI: "Human",
-        hideAIDifficultyChoice: true,
-        AIDifficulty: "Easy",
-        totalWins: 0},
+        {
+            id: 0,
+            name: "Player 1",
+            marker: "X",
+            humanOrAI: "Human",
+            hideAIDifficultyChoice: true,
+            AIDifficulty: "Easy",
+            totalWins: 0
+        },
 
-        {id: 1,
-        name: "Player 2",
-        marker: "O",
-        humanOrAI: "Human",
-        hideAIDifficultyChoice: true,
-        AIDifficulty: "Easy",
-        totalWins: 0}],
+        {
+            id: 1,
+            name: "Player 2",
+            marker: "O",
+            humanOrAI: "Human",
+            hideAIDifficultyChoice: true,
+            AIDifficulty: "Easy",
+            totalWins: 0
+        }],
 
-    board: ['', '', '', 
-            '', '', '', 
-            '', '', ''],
+    board: ['', '', '',
+        '', '', '',
+        '', '', ''],
+    totalMarkedCells: 0,
     whoseTurn: 'X',
     disableBoard: true,
     gameInfo: "Click Start",
 
     hideUnChecked: false
 });
-
 
 class TicTacToe extends Component {
     constructor(props) {
@@ -53,65 +58,167 @@ class TicTacToe extends Component {
     }
 
     startClicked() {
-        this.setState({hideUnChecked: true});
-        this.setState({disableBoard: false});
+        this.setState({ hideUnChecked: true });
+        this.setState({ disableBoard: false });
+
+        if (this.state.Players[0].humanOrAI === 'AI') {
+            this.AIMove();
+        }
     }
 
     resetClicked() {
         console.log('reset');
 
-        this.setState({...buildInitialState()});
+        this.setState({ ...buildInitialState() });
     }
 
     onChangeName(event, id) {
         let players = [...this.state.Players];
         players[id].name = event.target.value;
-        this.setState({players});
+        this.setState({ players });
     }
 
     onChangeHumanOrAI(event, id) {
         let players = [...this.state.Players];
 
-        players[id].humanOrAI = event.target.value; 
-        
+        players[id].humanOrAI = event.target.value;
+
         if (event.target.value === "AI") {
             players[id].hideAIDifficultyChoice = false;
         } else {
             players[id].hideAIDifficultyChoice = true;
         }
 
-        this.setState({players});
+        this.setState({ players });
     }
 
     onChangeAIDifficulty(event, id) {
         let players = [...this.state.Players];
         players[id].AIDifficulty = event.target.value;
-        this.setState({players});
+        this.setState({ players });
     }
 
     cellClicked(event) {
-       let newBoard = [...this.state.board];
+        this.markCell(parseInt(event.target.id));
+    }
 
-       newBoard[parseInt(event.target.id)] = this.state.whoseTurn;
+    AIMove() {
+        // AI MOVES
+        if (this.state.Players[0].humanOrAI === 'AI') {
+            if (this.state.Players[0].AIDifficulty === "Easy") {
+                this.markCell(AI.Easy(this.state.board));
+            } else if (this.state.Players[0].AIDifficulty === "Medium") {
+                this.markCell(AI.Medium(this.state.board));
+            }
+        }
+    }
 
-       this.setState({board: newBoard});
+    markCell(index) {
+        if (this.state.board[index] === '') {
+            let newBoard = [...this.state.board];
+
+            newBoard[index] = this.state.whoseTurn;
+
+            this.setState({ board: newBoard });
+
+            this.setState({ totalMarkedCells: this.state.totalMarkedCells + 1 })
+        }
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (this.checkForWinner(this.state.whoseTurn)) {
+            if (this.state.Players[0].marker === this.state.whoseTurn) {
+                /// player 1 wins
+                console.log("player1 won");
+            } else {
+                // player 2 wins
+                console.log('Player2 won');
+            }
+
+            // check to avoid infinite loop
+            if (this.state.disableBoard === false) {
+                this.setState({ disableBoard: true });
+            }
+        } else {
+            this.nextPlayersTurn(prevState);
+        }
+    }
+
+    checkForWinner(whoseTurnMarker) {
+        if (this.state.totalMarkedCells === 9) {
+            // game ends in tie
+            console.log("tie")
+            return true;
+        }
+
+        if (this.checkForWinningLine() === whoseTurnMarker) {
+            return true;
+        }
+
+        return false;
+    }
+
+    checkForWinningLine() {
+        // horizontal check
+        for (let index = 0; index < 3; index++) {
+            let a = this.state.board[0 + (index * 3)];
+            let b = this.state.board[1 + (index * 3)];
+            let c = this.state.board[2 + (index * 3)];
+
+            if (a === b && a === c && b === c) {
+                return a;
+            }
+        }
+
+        // vertical check
+        for (let index = 0; index < 3; index++) {
+            let a = this.state.board[0 + index];
+            let b = this.state.board[3 + index];
+            let c = this.state.board[6 + index];
+
+            if (a === b && a === c && b === c) {
+                return a;
+            }
+        }
+
+        // upper left to lower right diagonal check 
+        if (this.state.board[0] === this.state.board[4] && this.state.board[0] === this.state.board[8] && this.state.board[4] === this.state.board[8]) {
+
+            return this.state.board[0];
+        }
+
+        // upper right to lower left diagonal check
+        if (this.state.board[2] === this.state.board[4] && this.state.board[2] === this.state.board[6] && this.state.board[4] === this.state.board[6]) {
+            return this.state.board[2];
+        }
+
+        return '';
+    }
+
+
+    nextPlayersTurn(prevState) {
+        if (this.state.whoseTurn === prevState.whoseTurn) {
+            const nextTurn = this.state.whoseTurn === this.state.Players[0].marker ? this.state.Players[1].marker : this.state.Players[0].marker;
+            
+            this.setState({ whoseTurn: nextTurn });
+        }
     }
 
     render() {
         return (
             <div>
                 <div className="gameInfo">
-                    <GameInfo gameInfo={this.state.gameInfo} startClicked={this.startClicked} resetClicked={this.resetClicked}/>
+                    <GameInfo gameInfo={this.state.gameInfo} startClicked={this.startClicked} resetClicked={this.resetClicked} />
                 </div>
-                
+
                 <div className="mainGrid">
-                    <Player Player={this.state.Players[0]} ID={0} changeName={this.onChangeName} 
-                            changeHumanOrAI={this.onChangeHumanOrAI} changeAIDifficulty={this.onChangeAIDifficulty}
-                            hideUnchecked={this.state.hideUnChecked}/>
-                    <GameBoard board={this.state.board} disableBoard={this.state.disableBoard} cellClicked={this.cellClicked}/>
-                    <Player Player={this.state.Players[1]} ID={1} changeName={this.onChangeName} 
-                            changeHumanOrAI={this.onChangeHumanOrAI} changeAIDifficulty={this.onChangeAIDifficulty}
-                            hideUnchecked={this.state.hideUnChecked}/>
+                    <Player Player={this.state.Players[0]} ID={0} changeName={this.onChangeName}
+                        changeHumanOrAI={this.onChangeHumanOrAI} changeAIDifficulty={this.onChangeAIDifficulty}
+                        hideUnchecked={this.state.hideUnChecked} />
+                    <GameBoard board={this.state.board} disableBoard={this.state.disableBoard} cellClicked={this.cellClicked} />
+                    <Player Player={this.state.Players[1]} ID={1} changeName={this.onChangeName}
+                        changeHumanOrAI={this.onChangeHumanOrAI} changeAIDifficulty={this.onChangeAIDifficulty}
+                        hideUnchecked={this.state.hideUnChecked} />
                 </div>
             </div>
         );
